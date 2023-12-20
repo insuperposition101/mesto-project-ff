5575375cd07195c5d06f139852d39d6e79cf43b5
Ksenia Saenko <saenko.ksusha@mail.ru>
2023-12-20 15:35:00 +0300

    first review

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+function createCard(name, link, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__title').textContent = name;
+    deleteButton.addEventListener('click', function(event) {
+        deleteCard(event);
+    });
+
+    return cardElement;
+}
+
+
+function deleteCard(event) {
+    const listItem = event.target.closest('.card');
+    listItem.remove();
+ }
+
+
+initialCards.forEach(function(elem) {
+    placesList.append(createCard(elem.name, elem.link, deleteCard))
+})
